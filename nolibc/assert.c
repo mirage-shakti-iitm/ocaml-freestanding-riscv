11b6c7cf8a4cc92fca72223020d8cf13f6fa3be6
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2020-09-14 21:51:50 +0530

    Renamed puts to puts_Assert, to avoid conflict

--- nolibc/assert.c
+++ nolibc/assert.c
@@ -6,18 +6,18 @@
  * abort as quickly as possible without triggering further errors.
  */
 
-static void puts(const char *s)
+static void puts_assert(const char *s)
 {
     riscv_write(s, strlen(s));
 }
 
 void _assert_fail(const char *file, const char *line, const char *e)
 {
-    puts(file);
-    puts(":");
-    puts(line);
-    puts(": Assertion `");
-    puts(e);
-    puts("' failed\n");
+    puts_assert(file);
+    puts_assert(":");
+    puts_assert(line);
+    puts_assert(": Assertion `");
+    puts_assert(e);
+    puts_assert("' failed\n");
     abort();
 }
