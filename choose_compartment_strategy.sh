bea3d03e4f3eb36cd7304b176aad0e84b2b8f3a5
Sai Venkata Krishnan <saiganesha5.svkv@gmail.com>
2020-10-11 10:02:14 +0530

    Remove asm rule

--- choose_compartment_strategy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -ex
-
-option=$1
-chosen_dir="compartment_allocations/strategy_${option}"
-nolibc_src_dir="nolibc"
-# asmrun_src_dir="runtime"
-
-# cp ${chosen_dir}/${asmrun_src_dir}/* ${asmrun_src_dir}/
-cp ${chosen_dir}/${nolibc_src_dir}/* ${nolibc_src_dir}/
