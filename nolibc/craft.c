f16ca4711d9d5cac30bc0e7ac3fb346836cb416c
SaiVK <saiganesha5.svkv@gmail.com>
2021-05-21 15:28:05 +0530

    Added craft and debug modules in Shakti-MS TCB

---
+++ nolibc/craft.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+__int128 craft(unsigned int ptr, unsigned int base, unsigned int bound, unsigned int id)
+{
+	//printf("Called craft ptr = %x, base = %x\n",ptr, base);
+	__int128 ret;
+	ret = ptr;
+	ret ^= ((__int128)id << 32);
+	ret ^= ((__int128)base << 64);
+	ret ^= ((__int128)bound << 96);
+	return ret;
+}
