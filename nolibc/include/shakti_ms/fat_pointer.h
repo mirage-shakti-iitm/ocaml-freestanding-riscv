39044f80f4953ac847515c8252d5887c4567feed
SaiVK <saiganesha5.svkv@gmail.com>
2021-07-10 16:04:31 +0530

    Fixed shakti_ms include path

---
+++ nolibc/include/shakti_ms/fat_pointer.h
@@ -0,0 +1,11 @@
+__int128 craft(unsigned int ptr, unsigned int base, unsigned int bound, unsigned int id);
+
+unsigned long long random64();
+
+unsigned long long hash(unsigned long long *cook_ptr);
+
+void val(unsigned long long hi, unsigned long long lo);
+
+void safefree(__int128 fpr);
+
+__int128 safemalloc(unsigned long s);
